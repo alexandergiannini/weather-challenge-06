8c4b5ea402c097da2a6d339c00351c79002e26a9
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-01-28 23:22:06 -0800

    added dynamic reporting for the current weather and five day weather

--- script.js
+++ script.js
@@ -55,19 +55,6 @@ textColumn1.appendChild(mySearchedCities)
 //searchedCity.values = loadCities()
 let apiKey = "43ec9b071b79e32f1a5fb7f0d1a5d468"
 
-//api endpoint --> api.openweathermap.org
-let getCurrentWeather = function (argument) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
-    argument +
-    "&appid=" +
-    apiKey +
-    "&units=imperial"
-    )
-    .then(function (response) {
-        return response.json()
-    })
-}
-
 //`https://api.openweathermap.org/data/2.5/weather?q=Portland&appid=${apiKey}`
 //`https://api.openweathermap.org/data/2.5/weather?q=Portland&appid=43ec9b071b79e32f1a5fb7f0d1a5d468`
 
@@ -80,28 +67,38 @@ let myWeather = function () {
           return response.json();
         })
         .then(function(data) {
+
+            let currentWeatherSectionEl = document.querySelector(".current-weather-section");
+            currentWeatherSectionEl.innerHTML = '';
+
             console.log(data);
           let myCityName = document.createElement('p')
-          myCityName.textContent = searchedCity.textContent
-          displayedCityName.appendChild(myCityName)
+          myCityName.textContent = `${searchedCity.textContent} (${currentDay})`
+          myCityName.setAttribute('style', 'padding-top: 10px;')
+          currentWeatherSectionEl.appendChild(myCityName) //displayedCityName.appendChild(myCityName)
+
+          //need to fix this
+          let weatherIcon = document.createElement('img')
+          weatherIcon.setAttribute('src', `http://openweather.map.org/img/wn/${data.weather[0].icon}@2xpng`)
+          currentWeatherSectionEl.appendChild(weatherIcon)
 
           let temperature = document.createElement('p')
-          temperature.textContent = `Temperature: ${data.main.temp}ºf (${currentDay})`
-          displayedCityName.appendChild(temperature)
+          temperature.textContent = `Temperature: ${data.main.temp}ºf`
+           currentWeatherSectionEl.appendChild(temperature)                                            ///  displayedCityName.appendChild(temperature)
 
           let humidity = document.createElement('p')
           humidity.textContent = `Humidity: ${data.main.humidity}%` ///need to fix humidity
-          displayedCityName.appendChild(humidity)
+          currentWeatherSectionEl.appendChild(humidity)                           // displayedCityName.appendChild(humidity)
 
           let windSpeed = document.createElement('p')
           windSpeed.textContent = `Wind Speed: ${data.wind.speed} MPH`
-          displayedCityName.appendChild(windSpeed)
+          currentWeatherSectionEl.appendChild(windSpeed)                                    //  displayedCityName.appendChild(windSpeed)
 
 
             ///need to fix this and find the proper UV index property
-          let uvIndex = document.createElement('p')
-          uvIndex.textContent = `UV Index: ${data.current.uvi}`
-          displayedCityName.appendChild(uvIndex)
+         // let uvIndex = document.createElement('p')
+         // uvIndex.textContent = `UV Index: ${data.current.uvi}`
+          //displayedCityName.appendChild(uvIndex)
 
          // receiveCurrentWeather() ///may need to call a receiveCurrentWeather function here
         });
@@ -110,39 +107,102 @@ let myWeather = function () {
 let fiveDayWeather = function () {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity.textContent}&appid=${apiKey}&units=imperial`
     )
-    .then(function (response) {
+    .then(function (response) { 
         return response.json();
     })
     .then(function (data) {
         console.log(data)
 
+        fiveDayWeatherSection.innerHTML = ''
+
+        let tomorrowContainer = document.createElement('div')
+        tomorrowContainer.className = 'container'
+        tomorrowContainer.setAttribute('style', 'position: absolute; right: 500px;') //padding-left: 10px; ///be wary of this right property
+        ///may have to set attribute for the tomorrow Container
+        fiveDayWeatherSection.appendChild(tomorrowContainer)
+
+        let tomorrowRow = document.createElement('div')
+        tomorrowRow.className = 'row'
+        tomorrowRow.setAttribute('style', 'border: 1px solid; border-color: black; padding-right: 120px;')
+        tomorrowContainer.appendChild(tomorrowRow)
+
+
+        let tomorrowCol = document.createElement('div')
+        tomorrowCol.className = 'col'
+        tomorrowRow.appendChild(tomorrowCol)
+
+
+
         let tomorrowDate = document.createElement('p')
         tomorrowDate.textContent = tomorrow //`${data.list[6].dt_txt}`
         tomorrowDate.setAttribute('style', 'font-weight: bold; text-decoration: underline;')
-        fiveDayWeatherSection.appendChild(tomorrowDate)
+        tomorrowCol.appendChild(tomorrowDate) //tomorrowBox.appendChild(tomorrowDate)   fiveDayWeatherSection.appendChild(tomorrowDate)
 
         let tomorrowTemp = document.createElement('p')
-        tomorrowTemp.textContent = `Temp: ${data.list[6].main.temp} ºf`
-        fiveDayWeatherSection.appendChild(tomorrowTemp)
+        tomorrowTemp.textContent = `Temp: ${data.list[6].main.temp}ºf`
+        tomorrowCol.appendChild(tomorrowTemp) //fiveDayWeatherSection.appendChild(tomorrowTemp)
 
         let tomorrowHumidity = document.createElement('p')
-        tomorrowHumidity.textContent = `Humidity: ${data.list[6].main.humidity} %`
-        fiveDayWeatherSection.appendChild(tomorrowHumidity)
+        tomorrowHumidity.textContent = `Humidity: ${data.list[6].main.humidity}%`
+        tomorrowCol.appendChild(tomorrowHumidity) //fiveDayWeatherSection.appendChild(tomorrowHumidity)
+
+
+
+
+        let twoDaysFromNowContainer = document.createElement('div')
+        twoDaysFromNowContainer.className = 'container'
+        twoDaysFromNowContainer.setAttribute('style','padding-right: 50px; position: absolute; left: 200px')
+        ///may have to set attribute for the tomorrow Container
+        fiveDayWeatherSection.appendChild(twoDaysFromNowContainer)
+
+        let twoDaysFromNowRow = document.createElement('div')
+        twoDaysFromNowRow.className = 'row'
+        twoDaysFromNowRow.setAttribute('style', 'border: 1px solid; border-color: black; padding-right: 120px;')
+        twoDaysFromNowContainer.appendChild(twoDaysFromNowRow)
+
+
+        let twoDaysFromNowCol = document.createElement('div')
+        twoDaysFromNowCol.className = 'col'
+        twoDaysFromNowRow.appendChild(twoDaysFromNowCol)
+
 
 
 
         let twoDaysFromNowDate = document.createElement('p')
         twoDaysFromNowDate.textContent = twoDays//`${data.list[14].dt_txt}`
         twoDaysFromNowDate.setAttribute('style', 'font-weight: bold; text-decoration: underline;')
-        fiveDayWeatherSection.appendChild(twoDaysFromNowDate)
+        twoDaysFromNowCol.appendChild(twoDaysFromNowDate) //fiveDayWeatherSection.appendChild(twoDaysFromNowDate)
 
         let twoDaysFromNowTemp = document.createElement('p')
-        twoDaysFromNowTemp.textContent = `Temp: ${data.list[14].main.temp} ºf`
-        fiveDayWeatherSection.appendChild(twoDaysFromNowTemp)
+        twoDaysFromNowTemp.textContent = `Temp: ${data.list[14].main.temp}ºf`
+        twoDaysFromNowCol.appendChild(twoDaysFromNowTemp) //fiveDayWeatherSection.appendChild(twoDaysFromNowTemp)
 
         let twoDaysFromNowHumidity = document.createElement('p')
-        twoDaysFromNowHumidity.textContent = `Humidity: ${data.list[14].main.humidity} %`
-        fiveDayWeatherSection.appendChild(twoDaysFromNowHumidity)
+        twoDaysFromNowHumidity.textContent = `Humidity: ${data.list[14].main.humidity}%`
+        twoDaysFromNowCol.appendChild(twoDaysFromNowHumidity) //fiveDayWeatherSection.appendChild(twoDaysFromNowHumidity)
+
+
+
+
+
+
+        let threeDaysFromNowContainer = document.createElement('div')
+        threeDaysFromNowContainer.className = 'container'
+        threeDaysFromNowContainer.setAttribute('style','padding-right: 50px; position: absolute; left: 400px')
+        ///may have to set attribute for the tomorrow Container
+        fiveDayWeatherSection.appendChild(threeDaysFromNowContainer)
+
+        let threeDaysFromNowRow = document.createElement('div')
+        threeDaysFromNowRow.className = 'row'
+        threeDaysFromNowRow.setAttribute('style', 'border: 1px solid; border-color: black; padding-right: 120px;')
+        threeDaysFromNowContainer.appendChild(threeDaysFromNowRow)
+
+
+        let threeDaysFromNowCol = document.createElement('div')
+        threeDaysFromNowCol.className = 'col'
+        threeDaysFromNowRow.appendChild(threeDaysFromNowCol)
+
+
 
 
 
@@ -150,15 +210,34 @@ let fiveDayWeather = function () {
         let threeDaysFromNowDate = document.createElement('p')
         threeDaysFromNowDate.textContent = threeDays//`${data.list[22].dt_txt}`
         threeDaysFromNowDate.setAttribute('style', 'font-weight: bold; text-decoration: underline;')
-        fiveDayWeatherSection.appendChild(threeDaysFromNowDate)
+        threeDaysFromNowCol.appendChild(threeDaysFromNowDate) //fiveDayWeatherSection.appendChild(threeDaysFromNowDate)
 
         let threeDaysFromNowTemp = document.createElement('p')
-        threeDaysFromNowTemp.textContent = `Temp: ${data.list[22].main.temp} ºf`
-        fiveDayWeatherSection.appendChild(threeDaysFromNowTemp)
+        threeDaysFromNowTemp.textContent = `Temp: ${data.list[22].main.temp}ºf`
+        threeDaysFromNowCol.appendChild(threeDaysFromNowTemp)  ///fiveDayWeatherSection.appendChild(threeDaysFromNowTemp)
 
         let threeDaysFromNowHumidity = document.createElement('p')
-        threeDaysFromNowHumidity.textContent = `Humidity: ${data.list[22].main.humidity} %`
-        fiveDayWeatherSection.appendChild(threeDaysFromNowHumidity)
+        threeDaysFromNowHumidity.textContent = `Humidity: ${data.list[22].main.humidity}%`
+        threeDaysFromNowCol.appendChild(threeDaysFromNowHumidity) ///fiveDayWeatherSection.appendChild(threeDaysFromNowHumidity)
+
+
+
+        let fourDaysFromNowContainer = document.createElement('div')
+        fourDaysFromNowContainer.className = 'container'
+        fourDaysFromNowContainer.setAttribute('style','padding-right: 50px; position: absolute; left: 600px')
+        ///may have to set attribute for the tomorrow Container
+        fiveDayWeatherSection.appendChild(fourDaysFromNowContainer)
+
+        let fourDaysFromNowRow = document.createElement('div')
+        fourDaysFromNowRow.className = 'row'
+        fourDaysFromNowRow.setAttribute('style', 'border: 1px solid; border-color: black; padding-right: 120px;')
+        fourDaysFromNowContainer.appendChild(fourDaysFromNowRow)
+
+
+        let fourDaysFromNowCol = document.createElement('div')
+        fourDaysFromNowCol.className = 'col'
+        fourDaysFromNowRow.appendChild(fourDaysFromNowCol)
+
 
 
 
@@ -166,43 +245,50 @@ let fiveDayWeather = function () {
         let fourDaysFromNowDate = document.createElement('p')
         fourDaysFromNowDate.textContent = fourDays//`${data.list[30].dt_txt}`
         fourDaysFromNowDate.setAttribute('style', 'font-weight: bold; text-decoration: underline;')
-        fiveDayWeatherSection.appendChild(fourDaysFromNowDate)
+        fourDaysFromNowCol.appendChild(fourDaysFromNowDate)  //fiveDayWeatherSection.appendChild(fourDaysFromNowDate)
 
         let fourDaysFromNowTemp = document.createElement('p')
-        fourDaysFromNowTemp.textContent = `Temp: ${data.list[30].main.temp} ºf`
-        fiveDayWeatherSection.appendChild(fourDaysFromNowTemp)
+        fourDaysFromNowTemp.textContent = `Temp: ${data.list[30].main.temp}ºf`
+        fourDaysFromNowCol.appendChild(fourDaysFromNowTemp)  //fiveDayWeatherSection.appendChild(fourDaysFromNowTemp)
 
         let fourDaysFromNowHumidity = document.createElement('p')
-        fourDaysFromNowHumidity.textContent = `Humidity: ${data.list[30].main.humidity} %`
-        fiveDayWeatherSection.appendChild(fourDaysFromNowHumidity)
+        fourDaysFromNowHumidity.textContent = `Humidity: ${data.list[30].main.humidity}%`
+        fourDaysFromNowCol.appendChild(fourDaysFromNowHumidity)  ///fiveDayWeatherSection.appendChild(fourDaysFromNowHumidity)
 
+
+
+
+
+
+        let fiveDaysFromNowContainer = document.createElement('div')
+        fiveDaysFromNowContainer.className = 'container'
+        fiveDaysFromNowContainer.setAttribute('style','padding-right: 50px; position: absolute; left: 800px')
+        ///may have to set attribute for the tomorrow Container
+        fiveDayWeatherSection.appendChild(fiveDaysFromNowContainer)
+
+        let fiveDaysFromNowRow = document.createElement('div')
+        fiveDaysFromNowRow.className = 'row'
+        fiveDaysFromNowRow.setAttribute('style', 'border: 1px solid; border-color: black; padding-right: 120px;')
+        fiveDaysFromNowContainer.appendChild(fiveDaysFromNowRow)
+
+
+        let fiveDaysFromNowCol = document.createElement('div')
+        fiveDaysFromNowCol.className = 'col'
+        fiveDaysFromNowRow.appendChild(fiveDaysFromNowCol)
 
 
 
         let fiveDaysFromNowDate = document.createElement('p')
         fiveDaysFromNowDate.textContent = fiveDays//`${data.list[38].dt_txt}`
         fiveDaysFromNowDate.setAttribute('style', 'font-weight: bold; text-decoration: underline;')
-        fiveDayWeatherSection.appendChild(fiveDaysFromNowDate)
+        fiveDaysFromNowCol.appendChild(fiveDaysFromNowDate) //fiveDayWeatherSection.appendChild(fiveDaysFromNowDate)
 
         let fiveDaysFromNowTemp = document.createElement('p')
-        fiveDaysFromNowTemp.textContent = `Temp: ${data.list[38].main.temp} ºf`
-        fiveDayWeatherSection.appendChild(fiveDaysFromNowTemp)
+        fiveDaysFromNowTemp.textContent = `Temp: ${data.list[38].main.temp}ºf`
+        fiveDaysFromNowCol.appendChild(fiveDaysFromNowTemp) //fiveDayWeatherSection.appendChild(fiveDaysFromNowTemp)
 
         let fiveDaysFromNowHumidity = document.createElement('p')
-        fiveDaysFromNowHumidity.textContent = `Humidity: ${data.list[38].main.humidity} %`
-        fiveDayWeatherSection.appendChild(fiveDaysFromNowHumidity)
+        fiveDaysFromNowHumidity.textContent = `Humidity: ${data.list[38].main.humidity}%`
+        fiveDaysFromNowCol.appendChild(fiveDaysFromNowHumidity) //fiveDayWeatherSection.appendChild(fiveDaysFromNowHumidity)
     })
 }
-
-//myWeather()
-
-//let receiveCurrentWeather = function () {
-   // let myCityName = document.createElement('p')
-   // myCityName.textContent = searchedCity.textContent
-  //  displayedCityName.appendChild(myCityName)
-
-   // displayedCityName.textContent= searchedCity.textContent
-   // console.log(displayedCityName)
-//}
-
-//receiveCurrentWeather()
